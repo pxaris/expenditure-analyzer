3bacec01ace711da543a3dad04e34d5a7cf62301
pxaris <pxaris@gmail.com>
2024-11-04 17:45:33 +0200

    rename sample file

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ DATA_DIR = os.path.join(ROOT_DIR, 'data')
 REPORT_DIR = os.path.join(ROOT_DIR, 'report')
 
 # data specific information
-FILENAME = 'Κινήσεις κάρτας.csv'
-N_SKIPROWS = 6  # rows of the CSV file to skip from the beginning
+FILENAME = 'sample_data.csv'
+N_SKIPROWS = 6  # rows of the CSV file to skip
 DATE_COLUMN = 'Ημ/νία συναλλαγής'
 EXPENDITURE_COLUMN = 'Ποσό (EUR)'
 EXPENDITURE_CATEGORY_COLUMN = 'Κατηγορία δαπάνης'
