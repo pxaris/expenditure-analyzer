4764ec6be2d3a26be1eae22e216c39e0eace28b7
pxaris <pxaris@gmail.com>
2024-11-04 16:19:51 +0200

    add config file

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
+DATA_DIR = os.path.join(ROOT_DIR, 'data')
+REPORT_DIR = os.path.join(ROOT_DIR, 'report')
+
+FILENAME = 'Κινήσεις κάρτας.csv'
+N_SKIPROWS = 6
